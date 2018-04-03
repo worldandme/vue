09d747fccef7587cc506cc851bf6618d0bb51b4e
wordlandme <1007330936@qq.com>
2018-04-03 14:59:24 +0800

    vuex 小试牛刀

---
+++ src/components/Count.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr>
+        <h3>{{$store.state.count}}</h3>
+        <button @click="$store.commit('add')">+</button>
+        <button @click="$store.commit('reduce')">-</button>
+    </div>
+</template>
+<script>
+    import store from '@/vuex/store'
+    export default{
+        data(){
+            return{
+                msg:'Hello vuex'
+            }
+        },
+        store
+    }
+</script>
