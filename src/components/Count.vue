e450e2fc63dd01a7c9609b1b60776ed6ce9c411f
wordlandme <1007330936@qq.com>
2018-04-03 18:41:32 +0800

    vuex study

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -2,19 +2,49 @@
     <div>
         <h2>{{msg}}</h2>
         <hr>
-        <h3>{{$store.state.count}}</h3>
-        <button @click="$store.commit('add')">+</button>
-        <button @click="$store.commit('reduce')">-</button>
+        <h3>{{count}}</h3>
+        <button @click="$store.commit('add',10)">+</button>
+        <button @click="reduce">-</button>
+        <p>
+            <button @click="addAction">+</button>
+            <button @click="reduceAction">-</button>
+        </p>
+        <h3>{{$store.state.a.count}}</h3>
     </div>
 </template>
 <script>
     import store from '@/vuex/store'
+    import {mapState,mapMutations,mapGetters, mapActions} from 'vuex'
     export default{
         data(){
             return{
                 msg:'Hello vuex'
             }
         },
-        store
+        store,
+        // vuex向模板传值的方式一：
+            // computed: {
+            //     count(){
+            //         return this.$store.state.count;
+            //     }
+            // },
+        // vuex向模板传值的方式二：
+            // computed:mapState({
+            //     count:state=>state.count
+            // })
+        // vuex向模板传值的方式三：
+        computed:{
+            ...mapGetters(['count']),
+            // count(){
+            //     return this.$store.getters.count
+            // }
+        },
+        // methods:mapMutations(['add','reduce'])
+        methods:{
+            ...mapMutations([
+                'add','reduce'
+            ]),
+            ...mapActions(['addAction','reduceAction'])
+        }
     }
 </script>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -7,15 +7,29 @@ const state = {
     count:1
 }
 const mutations = {
-    add(state){
-        state.count++;
+    add(state,n){
+        state.count+=n;
     },
     reduce(state){
         state.count--;
     }
 }
-
+const getters = {
+    count:function(state){
+        return state.count += 10
+    }
+}
+const actions = {
+    addAction(context){
+        context.commit('add',10)
+    },
+    reduceAction({commit}){
+        commit('reduce')
+    }
+}
+const moduleA = {
+    state,mutations,getters,actions
+}
 export default new Vuex.Store({
-    state,
-    mutations
+    modules:{a:moduleA}
 })
